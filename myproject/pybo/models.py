2327e530b953b767317f99136bf4f130a1aa34fe
5dora <5dorabangs@gmail.com>
2023-04-14 16:20:34 +0900

    추천, 검색 기능 추가

--- myproject/pybo/models.py
+++ myproject/pybo/models.py
@@ -1,5 +1,12 @@
 from pybo import db
 
+# question_voter 테이블 객체 생성
+question_voter = db.Table(
+    'question_voter',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('question_id', db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'), primary_key=True)
+)
+
 
 # Quetion이 이제 테이블명 db.Model을 상속받아
 class Question(db.Model):
@@ -11,7 +18,14 @@ class Question(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=True, server_default='1')  # 그러나 이미 있던 데이터에는 user_id 값이 없다. nullable을 False로 해두면 오류가 난다. server_default는 null 대신 임의의 값을 넣어준다. server_default를 사용하면 flask db upgrade 명령을 수행할 때 null이던 데이터에도 기본값이 저장된다. default를 새로 생성되는 데이터에만 기본값을 생성해준다.
     user = db.relationship('User', backref=db.backref('question_set'))
     modify_date = db.Column(db.DateTime(), nullable=True)
+    voter = db.relationship('User', secondary=question_voter, backref=db.backref('question_voter_set'))
 
+
+answer_voter = db.Table(
+    'answer_voter',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True),
+    db.Column('answer_id', db.Integer, db.ForeignKey('answer.id', ondelete='CASCADE'), primary_key=True)
+)
 
 
 class Answer(db.Model):
@@ -25,7 +39,7 @@ class Answer(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=True, server_default='1')
     user = db.relationship('User', backref=db.backref('answer_set'))
     modify_date = db.Column(db.DateTime(), nullable=True)
-
+    voter = db.relationship('User', secondary=answer_voter, backref=db.backref('answer_voter_set'))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- myproject/pybo/views/question_views.py
+++ myproject/pybo/views/question_views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template, request, url_for, g, flash
 from werkzeug.utils import redirect
 
 from pybo import db
-from pybo.models import Question
+from pybo.models import Question, Answer, User
 
 from pybo.forms import QuestionForm, AnswerForm
 
@@ -15,10 +15,32 @@ bp = Blueprint('question', __name__, url_prefix='/question')
 
 @bp.route('/list/')
 def _list():
-    page = request.args.get('page', type=int, default=1)  # 페이지 값이 없을 경우 기본값 1로 세팅
+    page = request.args.get('page', type=int, default=1)
+    kw = request.args.get('kw', type=str, default='')
     question_list = Question.query.order_by(Question.create_date.desc())
+    if kw:
+        search = '%%{}%%'.format(kw)
+        sub_query = db.session.query(Answer.question_id, Answer.content, User.username) \
+            .join(User, Answer.user_id == User.id).subquery()
+        question_list = question_list \
+            .join(User) \
+            .outerjoin(sub_query, sub_query.c.question_id == Question.id) \
+            .filter(Question.subject.ilike(search) |  # 질문제목
+                    Question.content.ilike(search) |  # 질문내용
+                    User.username.ilike(search) |  # 질문작성자
+                    sub_query.c.content.ilike(search) |  # 답변내용
+                    sub_query.c.username.ilike(search)  # 답변작성자
+                    ) \
+            .distinct()
     question_list = question_list.paginate(page=page, per_page=10)
-    return render_template('question/question_list.html', question_list=question_list)
+    return render_template('question/question_list.html', question_list=question_list, page=page, kw=kw)
+
+# @bp.route('/list/')
+# def _list():
+#     page = request.args.get('page', type=int, default=1)  # 페이지 값이 없을 경우 기본값 1로 세팅
+#     question_list = Question.query.order_by(Question.create_date.desc())
+#     question_list = question_list.paginate(page=page, per_page=10)
+#     return render_template('question/question_list.html', question_list=question_list)
 
 
 @bp.route('/detail/<int:question_id>/')
@@ -73,3 +95,15 @@ def delete(question_id):
     db.session.delete(question)
     db.session.commit()
     return redirect(url_for('question._list'))
+
+
+@bp.route('/vote/<int:question_id>/')
+@login_required
+def vote(question_id):
+    _question = Question.query.get_or_404(question_id)
+    if g.user == _question.user:
+        flash('본인이 작성한 글은 추천할수 없습니다')
+    else:
+        _question.voter.append(g.user)
+        db.session.commit()
+    return redirect(url_for('question.detail', question_id=question_id))
